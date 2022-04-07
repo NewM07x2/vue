3d5534b0d45faeae1d8e2ec4e9c3638d39b8be24
masato_nitta <masato.nitta@g.softbank.co.jp>
2022-04-07 10:23:59 +0900

    sample update

--- sample/src/constants/const.js
+++ sample/src/constants/const.js
@@ -1,7 +1,4 @@
 // 画面表示メッセージ
 export const MESSAGES = {
-  USER_SEARCH_NOT_SELECT: 'ユーザーを選択してください。',
-
-  // 画面側入力チェック用メッセージ
-  W_CM_SP0002: '最大選択件数[{0}]を超えています。選択件数を絞ってください。'
+  USER_SEARCH_NOT_SELECT: 'エラーが存在しています。',
 }
---
+++ sample/src/constants/url.js
@@ -0,0 +1,7 @@
+// API URL
+export default {
+  // REMOVE DATA (tb_trn_isdoc, tb_trn_isdoc_catalog, tb_his_is_task_list, NAS)
+  REMOVE_DATA: `${process.env.DUKE_UI_DOMAIN}indoor-sim-ui/indoor-sim/master/delete/`,
+  // DELETE SPEC on serach screen (tb_trn_isdoc, tb_trn_isdoc_catalog, tb_his_is_task_list, NAS)
+  DELETE_SPEC: `${process.env.DUKE_UI_DOMAIN}indoor-sim-ui/indoor-sim/master/delete-spec`,
+}
